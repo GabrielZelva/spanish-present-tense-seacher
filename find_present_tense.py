0b3f7bdbe7bdd003fe8fcb9221120aa0d8e2af46
GabrielZelva <66466481+GabrielZelva@users.noreply.github.com>
2026-01-03 21:52:16 +0100

    Sentence splitting
    The program can now take in a whole file at once

--- find_present_tense.py
+++ find_present_tense.py
@@ -6,20 +6,45 @@ with open("hmm_model.pkl", "rb") as f:
     tagger = model_data["tagger"]
     vocab = model_data["vocab"]
 
-original_sentence = "El perro corre por la escalera."
-sentence = re.findall(r'\w+|[^\w\s]', original_sentence)
+corpus = "El perro corre por la escalera. El dueño se enfadará. El gato lo mira. No lo veo saliedo por la calle. Si durmieras bien, no tendrías pesadillas. Lo que sí, estoy muy seguro de que va a salir bien." 
 
-sentence = [w if w in vocab else "UNK_TOKEN" for w in sentence]
-tagged_sentence = tagger.tag(sentence)
+text_type = "normal"
 
+normal_text_pattern = r"[A-ZÁÉÍÓÚÑ][^.]*\." # This may need to be improved
+preseea_text_pattern = r"[A-Z]{1}:.*.$" # And this as well 
+
+present_pattern = r"[a-záéíóúñA-ZÁÉÍÓÚÑ]+((?<!(ad)|(id)|(and))o|(?<!(ab)|(í)|(ar))as|(?<!(ab)|(í)|(ar))a|(?<!(ab)|(í)|(ár))amos|áis|(?<!(ab)|(í)|(ar))an|[^s]es|[^s]e|[^rs]emos|[^r]éis|[^s]en|imos|ís|y){1}$"
 interest = []
 
-pattern = r"[a-záéíóúñA-ZÁÉÍÓÚÑ]+((?<!(ad)|(id)|(and))o|(?<!(ab)|(í)|(ar))as|(?<!(ab)|(í)|(ar))a|(?<!(ab)|(í)|(ár))amos|áis|(?<!(ab)|(í)|(ar))an|[^s]es|[^s]e|[^rs]emos|[^r]éis|[^s]en|imos|ís|y){1}$"
+if text_type == "normal":
 
-for token, tag in tagged_sentence:
-    if tag == "VERB":
-        if re.fullmatch(pattern, token):
+    sentences = re.findall(normal_text_pattern, corpus)
 
-            interest.append(original_sentence)
+elif text_type == "preseea":
 
-print(interest)
+    sentences = re.findall(preseea_text_pattern, corpus)
+
+for sentence in sentences:
+
+    tokens = re.findall(r'\w+|[^\w\s]', sentence)
+
+    tokens = [w if w in vocab else "UNK_TOKEN" for w in tokens]
+    tagged_tokens = tagger.tag(tokens)
+
+    for token, tag in tagged_tokens:
+        if tag == "VERB" and re.fullmatch(present_pattern, token):
+            interest.append(sentence)
+
+
+
+print(20*"#")
+print("Corpus:")
+print(20*"#")
+print(corpus)
+print(20*"#")
+print("\n")
+print(20*"#")
+print("Present tenses found:")
+print(20*"#")
+print("\n".join(interest))
+print(20*"#")
